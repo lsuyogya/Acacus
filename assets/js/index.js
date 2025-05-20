376bccc3b568d8a8f5f3f0e180d15f7899da7b10
Suyogya Luitel <Lsuyogya@gmail.com>
2025-05-20 08:53:49 +0545

    init: gradient effect

---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+  scrambleText("#scrambleText");
+  gradientMove();
+};
+
+function scrambleText(id) {
+  const textElement = document.querySelector(id);
+  const originalText = textElement.innerText;
+
+  gsap.registerPlugin(ScrambleTextPlugin);
+
+  const animateScramble = () => {
+    gsap.to(textElement, {
+      duration: 1,
+      scrambleText: {
+        text: originalText,
+        chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()",
+        revealDelay: 0.5,
+        speed: 2,
+      },
+    });
+  };
+
+  // Initial animation on page load
+  setTimeout(animateScramble, 500);
+
+  // Trigger on hover
+  textElement.addEventListener("pointerover", animateScramble);
+
+  //   // Trigger on focus (keyboard nav)
+  //   textElement.setAttribute("tabindex", "0"); // make it focusable
+  //   textElement.addEventListener("focus", animateScramble);
+}
+
+function gradientMove() {
+  const interBubble = document.querySelector(".interactive");
+  let curX = 0;
+  let curY = 0;
+  let tgX = 0;
+  let tgY = 0;
+
+  function move() {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    if (interBubble) {
+      interBubble.style.transform = `translate(${Math.round(
+        curX
+      )}px, ${Math.round(curY)}px)`;
+    }
+    requestAnimationFrame(move);
+  }
+
+  window.addEventListener("mousemove", (event) => {
+    tgX = event.clientX;
+    tgY = event.clientY;
+  });
+
+  move();
+}
